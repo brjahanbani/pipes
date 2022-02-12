932238532e391b677d6da959479db0e7a0bedee5
Babak <babak.jahanbani@yahoo.com>
2022-02-12 15:26:13 +0330

    Generate Pipe and Beautify UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  nameInput: string = '';
-  dateInput: string = '';
-  amountInput: string = '';
-
+  nameInput: string = 'Please type your complete name';
+  dateInput: string = '02/18/1988';
+  amountInput: number = 0;
+  mileInput: number = 0;
   onNameChange(value: string) {
     this.nameInput = value;
   }
   onDateChange(value: string) {
     this.dateInput = value;
   }
-  onAmountChange(value: string) {
+  onAmountChange(value: number) {
     this.amountInput = value;
+  }
+  onMileChange(value: number) {
+    this.mileInput = value;
   }
 }
