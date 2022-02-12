e88d58d7d1a5dcd09e6dcd1af6bedb588577f6b8
Babak <babak.jahanbani@yahoo.com>
2022-02-12 12:12:40 +0330

    Add Different Date Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  nameInput: string = '';
+  dateInput: string = '';
+
+  onNameChange(value: string) {
+    this.nameInput = value;
+  }
+  onDateChange(value: string) {
+    this.dateInput = value;
+  }
 }
